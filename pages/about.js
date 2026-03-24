7ad5788c018c40b48a1e18367d503eceef0f3917
Indrajit128 <indrajitkshirsagar128@gmail.com>
2026-03-24 13:24:08 +0530

    Decrease About page hero height

--- pages/about.js
+++ pages/about.js
@@ -38,7 +38,7 @@ export default function About() {
       </Head>
 
       {/* --- CINEMATIC HERO --- */}
-      <section className="relative h-[80vh] flex items-center justify-center overflow-hidden">
+      <section className="relative h-[60vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
             src="/images/Office2.webp"
